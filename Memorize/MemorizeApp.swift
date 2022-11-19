bce267b1e68871ea7911c3b72e6cc6522e20e522
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2022-11-19 13:47:54 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Yuriy Lutsenko on 19.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
