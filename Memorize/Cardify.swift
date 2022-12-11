d02bd4ade2066a41d2ac8932f1ab4a94583688cd
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2022-12-11 15:08:38 +0100

    Add PieShap

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,39 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Yuriy Lutsenko on 11.12.2022.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .center) {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            if isFaceUp {
+                shape.fill(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    .foregroundColor(.red)
+                content
+            } else {
+                shape.fill()
+                    .foregroundColor(.red)
+            }
+        }
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 10
+        static let lineWidth: CGFloat = 3
+    }
+    
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        return self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
