3f0e2e9f4acff570e0ce3ee38acf663f600ed67a
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2022-12-11 13:05:04 +0100

    Add PieShape

---
+++ Memorize/Pie.swift
@@ -0,0 +1,38 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by Yuriy Lutsenko on 11.12.2022.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool = false
+    
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        
+        let start = CGPoint(
+            x: rect.midX + radius * CGFloat(cos(startAngle.radians)),
+            y: rect.midY + radius * CGFloat(sin(startAngle.radians))
+        )
+        
+        let end = CGPoint(
+            x: rect.midX + radius * CGFloat(cos(endAngle.radians)),
+            y: rect.midY + radius * CGFloat(sin(endAngle.radians))
+        )
+        
+        var p = Path()
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: !clockwise)
+        p.addLine(to: center)
+        
+        return p
+    }
+    
+}
